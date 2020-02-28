c3ed02f0d51bc55d5348a56cdd685d6338379f9f
jiss02 <olo5112olo@naver.com>
2020-02-28 23:24:15 +0900

    Add emptyParmeter module

---
+++ Server-myAlgorithm/myAlgoritm/modules/utils/emptyParameter.js
@@ -0,0 +1,5 @@
+export function emptyParameter(datas){
+    const missParameter = Object.entries(datas)
+    .filter(it => it[1] == undefined).map(it => it[0]).join(',');
+    return missParameter
+}
