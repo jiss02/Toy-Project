dca88d9a7b976e8ae49762c6f62807cac72c05b4
jiss02 <olo5112olo@naver.com>
2020-02-29 04:06:43 +0900

    Add bodyparser code

--- Server-myAlgorithm/myAlgoritm/app.js
+++ Server-myAlgorithm/myAlgoritm/app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
