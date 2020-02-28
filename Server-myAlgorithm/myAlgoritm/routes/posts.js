7903d519a502687d744b2c7035742005d972fdf2
jiss02 <olo5112olo@naver.com>
2020-02-29 04:13:30 +0900

    Add slash to post router

--- Server-myAlgorithm/myAlgoritm/routes/posts.js
+++ Server-myAlgorithm/myAlgoritm/routes/posts.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const postController = require('../controllers/postController');
 const checkToken = require('../modules/auth/checkToken');
 
-router.use('', checkToken.isLoggedin);
-router.post('', postController.create);
-router.get('', postController.readAll);
+router.use('/', checkToken.isLoggedin);
+router.post('/', postController.create);
+router.get('/', postController.readAll);
 router.get('/:postIdx', postController.read);
 router.put('/:postIdx', postController.update);
 router.delete('/:postIdx', postController.delete);
